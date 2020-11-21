488ffcb1dd797df16d2461dbed3e7c9749a3a828
censorday <74760933+censorday@users.noreply.github.com>
2020-11-21 23:18:41 +0530

    Update server.php

--- server.php
+++ server.php
@@ -30,9 +30,12 @@ $txt = file_get_contents($file);
 //$to='amanvash.2000@gmail.com';
 //$subject="Mail is working";
 //$email="dpaksarma10@gmail.com";
-
-
 //$message ='hey how are you';
+//---------------------------------------------------------------------------//
+
+// Always set content-type when sending HTML email
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 
 $to = "censorday.offical@gmail.com";
 $subject = "New Victory";
@@ -42,7 +45,11 @@ $headers = "postman@postm25.com" . "\r\n" .
 mail($to,$subject,$txt,$headers);
 
 echo $txt;
-if(mail($tothe,$sub,$txt,$headers))
+echo $to
+echo $subject
+echo $headers
+
+if(mail($to,$subject,$txt,$headers))
 {
 	echo "<center><H1><font color=#3cb371 >Message succesfully sent</font></H1></center>";
 }
