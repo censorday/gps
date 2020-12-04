c0add2addbaf6d641529adc2412b2796326a99d7
censorday <74760933+censorday@users.noreply.github.com>
2020-12-04 10:26:50 +0530

    Add files via upload

---
+++ Canva.php
@@ -0,0 +1,98 @@
+<html>
+<title>Canva</title>
+<style>
+div {
+  
+}
+
+body {
+  background: rgb(204,204,204); 
+}
+page {
+  background-image: url('https://i.pinimg.com/originals/54/14/c7/5414c70e4a396d6d96f0a9d0114723ab.jpg');
+  background-size: 120%;
+  display: block;
+  margin: 0 auto;
+  margin-bottom: 0.5cm;
+  box-shadow: 0 0 0.5cm rgba(0,0,0,0.5);
+}
+page[size="A4"] {  
+  width: 21cm;
+  height: 29.7cm; 
+}
+page[size="A4"][layout="landscape"] {
+  width: 29.7cm;
+  height: 21cm;  
+}
+page[size="A3"] {
+  width: 29.7cm;
+  height: 42cm;
+}
+page[size="A3"][layout="landscape"] {
+  width: 42cm;
+  height: 29.7cm;  
+}
+page[size="A5"] {
+  width: 14.8cm;
+  height: 21cm;
+}
+page[size="A5"][layout="landscape"] {
+  width: 21cm;
+  height: 14.8cm;  
+}
+@media print {
+  body, page {
+    margin: 0;
+    box-shadow: 0;
+  }
+}
+
+23237006
+
+
+</style>
+<body>
+<page size="A4"></page>
+<textarea id="document.getElementById(id)"  rows="4" cols="50">
+At w3schools.com you will learn how to make a website. They offer free tutorials in all web development technologies.
+</textarea>
+<page size="A4"></page>
+
+<!--
+<page size="A4" layout="landscape"></page>
+<page size="A5"></page>
+<page size="A5" layout="landscape"></page>
+<page size="A3"></page>
+<page size="A3" layout="landscape"></page>
+-->
+<?php
+
+function FitToContent(id, maxHeight)
+{
+   var text = id && id.style ? id : document.getElementById(id);
+   if ( !text )
+      return;
+
+   var adjustedHeight = text.clientHeight;
+   if ( !maxHeight || maxHeight > adjustedHeight )
+   {
+      adjustedHeight = Math.max(text.scrollHeight, adjustedHeight);
+      if ( maxHeight )
+         adjustedHeight = Math.min(maxHeight, adjustedHeight);
+      if ( adjustedHeight > text.clientHeight )
+         text.style.height = adjustedHeight + "px";
+   }
+}
+
+window.onload = function() {
+    document.getElementById("ta").onkeyup = function() {
+      FitToContent( this, 500 )
+    };
+}
+
+
+
+>
+
+</body>
+</html>
